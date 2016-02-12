f6584d0ad0440ab99a8a7b312c60aa8f830538e8
a00926085 <a00926085@SE12L0310-17.ad.bcit.ca>
2016-02-11 17:28:24 -0800

    Finished Step 5

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -76,7 +76,7 @@ class Order extends Application {
 
     // add an item to an order
     function add($order_num, $item) {
-        //FIXME
+        $this->orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
