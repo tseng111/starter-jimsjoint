0926a2f0bcbd870337d273aa1cc145bc271f81ea
John Janzen <joalja2002@msn.com>
2016-02-11 17:42:19 -0800

    Stage 6 complete, new Upgrade Available

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -54,7 +54,16 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+        $CI = & get_instance();
+        $items=$CI->orderitems->group($num);
+        $gotem = array();
+        if (count($items) > 0) {
+            foreach($items as $item) {
+                $menu = $CI->menu->get($item->item);
+                $gotem[$menu->category] = 1;
+            }
+        }
+        return isset($gotem['m']) && isset($gotem['d']) && isset($gotem['s']);
     }
 
 }
