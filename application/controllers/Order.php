940aca72706fc9ce8513f1aadbd634e82dee9412
a00926085 <a00926085@SE12L0310-17.ad.bcit.ca>
2016-02-11 18:03:24 -0800

    Finished Step 7

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -95,19 +95,31 @@ class Order extends Application {
         }
         $this->data['items'] = $items;
         
-        $this->data['okornot'] = $this->orders->validate($order_num);
+        $this->data['okornot'] = $this->orders->validate($order_num) ? "" : "disabled";
+        
         $this->render();
     }
 
     // proceed with checkout
     function commit($order_num) {
         //FIXME
+        if(!$this->orders->validate($order_num))
+            redirect('/order/display_menu/' . $order_num);
+        $record = $this->orders->get($order_num);
+        $record->date = date(DATE_ATOM);
+        $record->status = 'c';
+        $record->total = $this->orders->total($order_num);
+        $this->orders->update($record);
         redirect('/');
     }
 
     // cancel the order
     function cancel($order_num) {
         //FIXME
+        $this->orderitems->delete_some($order_num);
+        $record = $this->orders->get($order_num);
+        $record->status = 'x';
+        $this->orders->update($record);
         redirect('/');
     }
 
