3d903c976b420241c2c716cb1247cdc4b018b18e
a00926085 <a00926085@SE12L0310-17.ad.bcit.ca>
2016-02-11 16:51:21 -0800

    Fixing commit issues

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,14 +17,6 @@ class Order extends Application {
 
     // start a new order
     function neworder() {
-        $order_num = $this->orders->highest() + 1;
-        
-        $neworder = $this->order->create();
-        $neworder->num = $order_num;
-        $neworder->date = date();
-        $neworder->status = 'a';
-        $neworder->total = 0;
-        $this->orders->add($neworder);
 
         redirect('/order/display_menu/' . $order_num);
     }
